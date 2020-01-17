dfcc38801021d2494fdddc4114c080037b0bebcf
lab314-15@labeng.local <lab314-15@labeng.local>
2020-01-17 16:29:29 +0700

    lab6

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,4 +1,33 @@
 //Dialog of the first example is given below.
+#include<iostream>
+using namespace std;
+main(){
+    string name,movie,when,quote;
+    int ID;
+    cout<<"Fahsai: Sawadee ka...Can you tell me your name?"<<endl;
+    cout<<"?????: ";
+    getline(cin,name);
+    cout<<"Fahsai: Wow!!! "<<name<<" that is a really cool name."<<endl<<"Fahsai: I think you are Engineering student. What is your student ID?"<<endl;
+    cout<<name<<": ";
+    cin>>ID;
+    cin.ignore();
+    cout<<"Fahsai: I think you may be GEAR "<<int((ID/10000000))-12<<" I have a free movie tickets for you."<<endl;
+    cout<<"Fahsai: Let's go to cinema together!!!"<<endl<<"Fahsai: What movie do you want to watch?"<<endl;
+    cout<<name<<": ";
+    getline(cin,movie);
+    cout<<"Fahsai: So....which day are you free to go with me?"<<endl;
+    cout<<name<<": ";
+    getline(cin,when);
+    cout<<"Fahsai: "<<when<<"....that is OK!!! I'm looking forward to watch "<<movie<<" with you."<<endl;
+    cout<<name<<": ";
+    getline(cin,quote);
+    cout<<"Fahsai: 555+ see you "<<when<<". Bye Bye \\(^ ^)/";
+
+
+
+
+
+}
 
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,11 +2,21 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	char y;
+	if(x>=65&&x<=90){
+		if(x==65){
+			return 'Z';
+		}else{
+			y=x-1;
+		}
+		return y;	
+		
+	}else{
+		return '0';
+	}
 }
 
 int main(){
-	//Test Case
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
